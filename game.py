977b6fd58a0fdb726d2d7fb3b48a6154f7644ae8
ikale1234 <55333848+ikale1234@users.noreply.github.com>
2020-08-21 19:28:40 -0700

    new game

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 width = 1000
 height = 900
 win = pygame.display.set_mode((width, height))
@@ -54,19 +55,98 @@ class Circle:
     def __init__(self):
         self.radius = 50
         self.color = (255, 0, 0)
+        self.visible = True
+        self.x = random.randrange(50, width-50)
+        self.y = random.randrange(100, height-50)
+
+    def draw(self):
+        if self.visible:
+            pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def changecolor(self, color):
+        self.color = color
+
+    def changepos(self):
+        self.x = random.randrange(50, width-50)
+        self.y = random.randrange(100, height-50)
 
 
 class Game:
     def __init__(self):
         self.run = True
-        self.bg_color = (0, 0, 0)
+        self.circlelist = []
+        self.bg_color = (255, 255, 255)
+        self.tooclose = False
+        for i in range(4):
+            self.circlelist.append(Circle())
 
-    def draw(self)
+    def checkcircledistance(self):
+        for i in range(len(self.circlelist)):
+            if self.tooclose:
+                break
+            for j in range(len(self.circlelist)):
+                self.xbetween = abs(
+                    self.circlelist[i].x - self.circlelist[j].x)
+                self.ybetween = abs(
+                    self.circlelist[i].y - self.circlelist[j].y)
+                if i != j:
+                    if (self.xbetween**2+self.ybetween**2)**0.5 < 150:
+                        for k in range(len(self.circlelist)):
+                            self.circlelist[k].changepos()
+                        self.tooclose = True
+                        break
+        while self.tooclose:
+            self.tooclose = False
+            for i in range(len(self.circlelist)):
+                if self.tooclose:
+                    break
+                for j in range(len(self.circlelist)):
+                    self.xbetween = abs(
+                        self.circlelist[i].x - self.circlelist[j].x)
+                    self.ybetween = abs(
+                        self.circlelist[i].y - self.circlelist[j].y)
+                    if i != j:
+                        if (self.xbetween**2+self.ybetween**2)**0.5 < 150:
+                            for k in range(len(self.circlelist)):
+                                self.circlelist[k].changepos()
+                            self.tooclose = True
+                            break
+
+        self.mousenotpressed = True
+
+    def drawgame(self):
+        for circle in self.circlelist:
+            circle.draw()
 
     def rungame(self):
+        self.checkcircledistance()
         while self.run:
             win.fill(self.bg_color)
             pygame.time.delay(10)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
+            self.x, self.y = pygame.mouse.get_pos()
+            pygame.event.get()
+            if pygame.mouse.get_pressed() == (0, 0, 0):
+                self.mousenotpressed = True
+            if pygame.mouse.get_pressed() == (1, 0, 0) and self.mousenotpressed:
+                for circle in self.circlelist:
+                    self.xdiff = abs(self.x-circle.x)
+                    self.ydiff = abs(self.y-circle.y)
+                    if (self.xdiff**2+self.ydiff**2)**0.5 < 50:
+                        for circle in self.circlelist:
+                            circle.changepos()
+                            self.checkcircledistance()
+
+                    self.mousenotpressed = False
+            elif pygame.mouse.get_pressed() == (1, 0, 0):
+                self.mousenotpressed = False
+            self.drawgame()
+
+            pygame.display.update()
+        pygame.quit()
+
+
+game = Game()
+game.rungame()
